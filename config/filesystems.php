1c5ddd26314d85618d97ef555c86c4e359c7009c
mlokli <petrov.alexandr.1997@gmail.com>
2020-08-23 18:53:33 +0300

    add css, js, files

--- config/filesystems.php
+++ config/filesystems.php
@@ -80,6 +80,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        public_path('image-project') => storage_path('app/images/projects'),
     ],
 
 ];
