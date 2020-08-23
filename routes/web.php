490e4e171c022793fdb1672d49a2f45b7e11671b
mlokli <petrov.alexandr.1997@gmail.com>
2020-08-23 18:38:14 +0300

    add router web

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
 });
+
+Route::resource('projects', 'ProjectController');
+
+Route::resource('logs', 'LogController');
